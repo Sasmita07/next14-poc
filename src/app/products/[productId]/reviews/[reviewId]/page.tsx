17a492028e36dd28a880ccbce33c79a7aea4adc1
Sasmita Ojha <sasmitaojha@Sasmitas-MacBook-Pro.local>
2024-10-06 22:10:47 +0530

    feat: catch all segments and not found page implementation

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,3 +1,8 @@
+import { notFound } from "next/navigation"
+
 export default function ReviewDetails({params} : { params: {productId: string, reviewId: string}}) {
+    if(parseInt(params.reviewId)> 1000){
+        notFound();
+    }
     return <h2>Review Details of product {params.productId} is {params.reviewId}</h2>
 }

63386e0ccd2cedefc02ff47b905ca5ace7a1c651
Sasmita Ojha <sasmitaojha@Sasmitas-MacBook-Pro.local>
2024-10-26 23:43:55 +0530

    feat: nextjs rendering startegy

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,12 @@
+import { cookies } from "next/headers";
+
 export const metadata = {
     "title": "About Section"
 }
 export  default function About() {
-    return <h2>About Page</h2>
+    console.log("Server Side");
+    const cookieStorage = cookies();
+    const theme = cookieStorage.get('theme');
+    console.log('theme--->', theme);
+    return <h2>About Page {new Date().toLocaleTimeString()}</h2>
 }
---
+++ src/components/property.tsx
@@ -0,0 +1,4 @@
+export const Property = async () => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    return <div>Property</div>;
+};
